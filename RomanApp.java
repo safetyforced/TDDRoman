f2cd2896611939b3c5db78431691628cda2133d5
Alex Marks <marks.alexanderj@gmail.com>
2016-06-14 22:51:27 -0400

    Two digit conversion working.  First attempts at modularity.

--- RomanApp.java
+++ RomanApp.java
@@ -2,32 +2,104 @@ package src;
 
 public class RomanApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static String numberConvert(int number) {
+		if (number / 10 == 0)
+			return oneDigitConvert(number);
+		if (number / 10 == 1)
+			return "X" + oneDigitConvert(number % 10, number / 10);
+		if (number / 10 == 2)
+			return "XX" + oneDigitConvert(number % 10, number / 10);
+		if (number / 10 == 3)
+			return "XXX" + oneDigitConvert(number % 10, number / 10);
+		if (number / 10 == 4)
+			return "XL" + oneDigitConvert(number % 10, number / 10);
+		if (number / 10 == 5)
+			return "L" + oneDigitConvert(number % 10, number / 10);
+		if (number / 10 == 6)
+			return "LX" + oneDigitConvert(number % 10, number / 10);
+		if (number / 10 == 7)
+			return "LXX" + oneDigitConvert(number % 10, number / 10);
+		if (number / 10 == 8)
+			return "LXXX" + oneDigitConvert(number % 10, number / 10);
+		if (number / 10 == 9)
+			return "XC" + oneDigitConvert(number % 10, number / 10);
+		else
+			return "";
 	}
 
-	public static String oneDigitConvert(int i) {
-		if (i == 1)
-			return "I";
-		if (i == 2)
-			return "II";
-		if (i == 3)
-			return "III";
-		if (i == 4)
-			return "IV";
-		if (i == 5)
-			return "V";
-		if (i == 6)
-			return "VI";
-		if (i == 7)
-			return "VII";
-		if (i == 8)
-			return "VIII";
-		if (i == 9)
-			return "IX";
+	public static String oneDigitConvert(int digit, int decimal) {
+		String romanDigit = "";
+		
+		if (decimal < 10) {
+			if (digit == 4)
+				return romanDigit + "IV";
+			if (digit == 9)
+				return romanDigit + "IX";
+			if (digit == 5)
+				return romanDigit + "V";
+			if (digit > 5 && digit < 10) {
+				romanDigit += "V";
+				for (int i = 0; i < (digit - 5); i++) {
+					romanDigit += "I";
+				}
+				return romanDigit;
+			}
+			if (digit == 10)
+				return romanDigit;
+			else
+				for (int i = 0; i < digit; i++) {
+					romanDigit += "I";
+				}
+			return romanDigit;
+		}
+		return romanDigit;
+	}
+
+	public static String oneDigitConvert(int digit) {
+		String romanDigit = "";
+
+		if (digit == 4)
+			return romanDigit + "IV";
+		if (digit == 9)
+			return romanDigit + "IX";
+		if (digit == 5)
+			return romanDigit + "V";
+		if (digit > 5 && digit < 10) {
+			romanDigit += "V";
+			for (int i = 0; i < (digit - 5); i++) {
+				romanDigit += "I";
+			}
+			return romanDigit;
+		}
+		if (digit >= 10)
+			return romanDigit;
 		else
-			return "nope";
+			for (int i = 0; i < digit; i++) {
+				romanDigit += "I";
+			}
+
+		return romanDigit;
+
+		// if (digit == 1)
+		// return romanDigit + "I";
+		// if (digit == 2)
+		// return romanDigit + "II";
+		// if (digit == 3)
+		// return romanDigit + "III";
+		// if (digit == 4)
+		// return romanDigit + "IV";
+		// if (digit == 5)
+		// return romanDigit + "V";
+		// if (digit == 6)
+		// return romanDigit + "VI";
+		// if (digit == 7)
+		// return romanDigit + "VII";
+		// if (digit == 8)
+		// return romanDigit + "VIII";
+		// if (digit == 9)
+		// return romanDigit + "IX";
+		// else
+		// return romanDigit;
 	}
 
 }
